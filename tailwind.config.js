22ef829b9d601d82a74c88df6d85487bf129f947
Ashish <ashishadhikari5255@gmail.com>
2024-10-01 22:43:42 +0545

    Complete styling skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,26 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: "15px",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)", // Fixed the variable reference
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00f99",
+          hover: "#00e187",
+        },
       },
     },
   },
